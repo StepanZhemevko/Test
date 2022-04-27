7aa58d7ed7e6a35d203a67fc3d16d2036de1204e
Stepan <steve.zop.das@gmail.com>
2022-04-28 00:30:44 +0300

    [TS-2] 10 time HW

--- simple-project/src/main/java/academy/com/App.java
+++ simple-project/src/main/java/academy/com/App.java
@@ -1,8 +1,10 @@
 package academy.com;
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+public class App {
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello World!");
+		}
+	}
 }
